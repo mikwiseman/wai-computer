800a90410c9b1baef74a71cba330fdb32f0ad20c
Mik Wiseman <hi@mikwiseman.com>
2026-05-18 19:20:21 +0300

    Bump release builds for icon refresh

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "is.waiwai.computer"
         minSdk = 28
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.1.3"
+        versionCode = 13
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
